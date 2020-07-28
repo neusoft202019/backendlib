3e6dbb18e342d54c63168530694c413f0b4d37ad
1173300917 <2387318022@qq.com>
2020-07-29 00:20:53 +0800

    更新配置成功

---
+++ HotelMangerOfNeu/src/test/java/com/hit/hotel/HotelMangerOfNeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hit.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMangerOfNeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
