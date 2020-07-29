fdb610b43037f00db9beab89e0a2574513734611
rootZWH <214704267@qq.com>
2020-07-29 09:29:35 +0800

    客房管理的房间与物品模块

--- HotelMangerOfNeu/src/main/java/com/hit/hotel/HotelMangerOfNeuApplication.java
+++ HotelMangerOfNeu/src/main/java/com/hit/hotel/HotelMangerOfNeuApplication.java
@@ -1,9 +1,11 @@
 package com.hit.hotel;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages={"com.hit.hotel.room.mapper"})
 public class HotelMangerOfNeuApplication {
 
 	public static void main(String[] args) {
